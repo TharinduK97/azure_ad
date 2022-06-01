766596e02aa696cbabd79b05e7315cafa012a113
Tharindu Kumarasinghe <57245404+TharinduK97@users.noreply.github.com>
2022-06-01 11:15:30 +0530

    initian commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useMsal } from "@azure/msal-react";
+import { loginRequest } from "./authConfig";
+import Button from "react-bootstrap/Button";
+
+function handleLogin(instance) {
+  instance.loginRedirect(loginRequest).catch(e => {
+    console.error(e);
+});
+}
 
 function App() {
+  const { instance } = useMsal();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Button variant="secondary" className="ml-auto" onClick={() => handleLogin(instance)}>Sign in using Popup</Button>
     </div>
   );
 }
